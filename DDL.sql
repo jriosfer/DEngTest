97f62f080e1d5c236468a2b5dd11ade8d0107072
jriosfer <jriosfer@gmail.com>
2024-12-17 17:55:14 -0500

    Added sequences, triggers

--- DDL.sql
+++ DDL.sql
@@ -106,3 +106,164 @@ ALTER TABLE job
 ALTER TABLE job
     ADD CONSTRAINT job_employee_fk FOREIGN KEY ( employee_employee_id )
         REFERENCES employee ( employee_id );
+		
+
+
+CREATE TABLE department_load (
+    record_id       number not null primary key,
+    deptno          VARCHAR2(25) NOT NULL,
+    department_name VARCHAR2(512),
+    creation_date   VARCHAR2(128) NOT NULL,
+    created_by      VARCHAR2(128) NOT NULL,
+    updated_date    VARCHAR2(128),
+    updated_by      VARCHAR2(128)
+);
+
+COMMENT ON COLUMN department_load.deptno IS
+    'Department Code';
+
+COMMENT ON COLUMN department_load.department_name IS
+    'Department Name or Business area name';
+
+COMMENT ON COLUMN department_load.creation_date IS
+    'Record''s creation date';
+
+COMMENT ON COLUMN department_load.created_by IS
+    'Record created  by';
+
+COMMENT ON COLUMN department_load.updated_date IS
+    'Record''s update  date';
+
+COMMENT ON COLUMN department_load.updated_by IS
+    'Record updated by';
+	
+
+
+CREATE TABLE employee_load (
+    record_number   number not null primary key,
+    employee_id   NUMBER NOT NULL,
+    name          VARCHAR2(512),
+    last_name     VARCHAR2(128),
+    middle_name   VARCHAR2(128),
+    creation_date VARCHAR2(128) NOT NULL,
+    created_by    VARCHAR2(128) NOT NULL,
+    updated_date  VARCHAR2(128),
+    updated_by    VARCHAR2(128)
+);
+
+COMMENT ON COLUMN employee_load.employee_id IS
+    'employee''s ID';
+
+COMMENT ON COLUMN employee_load.name IS
+    'Employees''s name';
+
+COMMENT ON COLUMN employee_load.last_name IS
+    'Last name';
+
+COMMENT ON COLUMN employee_load.middle_name IS
+    'Middle name if applies';
+
+COMMENT ON COLUMN employee_load.creation_date IS
+    'Record''s creation date';
+
+COMMENT ON COLUMN employee_load.created_by IS
+    'Record created  by';
+
+COMMENT ON COLUMN employee_load.updated_date IS
+    'Record''s update  date';
+
+COMMENT ON COLUMN employee_load.updated_by IS
+    'Record updated by';
+
+
+CREATE TABLE job_load (
+record_number number not null primary key,
+    deptno        VARCHAR2(25),
+    employee_id   NUMBER,
+    salary        VARCHAR2(128),
+    from_date     VARCHAR2(128),
+    until_date    VARCHAR2(128),
+    creation_date VARCHAR2(128) NOT NULL,
+    created_by    VARCHAR2(128) NOT NULL,
+    updated_date  VARCHAR2(128),
+    updated_by    VARCHAR2(128)
+);
+
+COMMENT ON COLUMN job_load.salary IS
+    'Salary amount (monthly)';
+
+COMMENT ON COLUMN job_load.from_date IS
+    'From the salary is valid';
+
+COMMENT ON COLUMN job_load.until_date IS
+    'Until  the salary is valid';
+
+COMMENT ON COLUMN job_load.creation_date IS
+    'Record''s creation date';
+
+COMMENT ON COLUMN job_load.created_by IS
+    'Record created  by';
+
+COMMENT ON COLUMN job_load.updated_date IS
+    'Record''s update  date';
+
+COMMENT ON COLUMN job_load.updated_by IS
+    'Record updated by';
+	
+	
+create sequence JOB_LOAD_NEXT
+start with 1
+increment by 1
+cache 20
+minvalue 1
+maxvalue 999999999999999999999999
+nocycle;
+
+
+create sequence EMPLOYEE_LOAD_NEXT
+start with 1
+increment by 1
+cache 20
+minvalue 1
+maxvalue 999999999999999999999999
+nocycle;
+
+
+create sequence DEPTNO_LOAD_NEXT
+start with 1
+increment by 1
+cache 20
+minvalue 1
+maxvalue 999999999999999999999999
+nocycle;	
+
+
+create or replace trigger DEPARTMENT_LOAD_BI
+before
+insert or update or delete on DEPARTMENT_LOAD
+for each row
+begin
+    :new.record_id := DEPTNO_LOAD_NEXT.nextval;
+	
+end;
+/
+
+create or replace trigger EMPLOYEE_LOAD_BI
+before
+insert or update or delete on EMPLOYEE_LOAD
+for each row
+begin
+    :new.record_number := EMPLOYEE_LOAD_NEXT.nextval;
+	
+end;
+/
+
+create or replace trigger JOB_LOAD_BI
+before
+insert or update or delete on JOB_LOAD
+for each row
+begin
+    :new.record_number := JOB_LOAD_NEXT.nextval;
+	
+end;
+/
